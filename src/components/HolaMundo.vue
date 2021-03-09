21f12d0dd167a8265ad5f2078d5c3b5021445c00
Gustavo Costa <gucostarp@gmail.com>
2021-03-09 17:18:02 -0300

    Commit inicial

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    Hola {{ nombre }} {{ edad }}!! | {{ triploIdade }}!
+    <button @click="showMessage">Mostrar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HolaMundo',
+  props: ['nombre', 'edad'],
+  computed: {
+    triploIdade() {
+      return this.edad * 3;
+    },
+  },
+  created() {
+    console.log(`foi criado ${this.nombre}`);
+  },
+  methods: {
+    showMessage() {
+      window.alert(`teste ${this.nombre}`);
+    },
+  },
+  data: () => ({
+    naombre: 'André',
+  }),
+};
+</script>
+
+<style>
+  .classezinha{
+    background-color: #984849;
+  }
+</style>
